83ee5901d361f96be518377436018dc033d8ee17
JesuGez <jesugonzalez210902@gmail.com>
2023-07-05 10:16:20 -0500

    first commit

---
+++ controller/novedadController.js
@@ -0,0 +1,53 @@
+const conexion = require('../config/conexion.js');
+const { check, validationResult } = require('express-validator');
+var express = require('express') //llamamos a Express
+const cors = require('cors');
+var rutas = express()  
+
+rutas.use(cors())
+//---------------- define las rutas de la API-------------------
+rutas.get('/', function(req, res) {
+    res.json({ mensaje: '¡Hola Mundo!' })  
+})
+
+//Guardar registro
+rutas.post('/registrar_novedad', [
+    check('nombre').isLength({min: 1}),
+    check('celular').isNumeric(),
+    check("correo").isEmail(),
+    check("direccion").isLength({ min: 1}),
+    check("novedad").isLength({ min: 1}),
+    check("nivel").isLength({ min: 1}),
+    check("usuario").isLength({ min: 1})
+], function(req, res) {
+    let sql = "insert into registro_novedad set ?"
+    const fecha = new Date
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array() })
+    }
+    console.log('Registro recibido: ',req.body);
+    let poststr = {
+        nombre: req.body.nombre,
+        celular : req.body.celular,
+        correo: req.body.correo,
+        direccion: req.body.direccion,
+        novedad: req.body.novedad,
+        nivel: req.body.nivel,
+        usuario: req.body.usuario,
+        modified: fecha
+    }
+   conexion.query(sql, poststr, function (error, results) {
+    if (error) throw error;
+    if (results.affectedRows) {
+     res.json({status: 'Registro guardado'})
+   }
+   else
+     res.json({status: 'No se pudo guardar'})
+    
+  });  
+})
+
+//---------------- Fin define las rutas de la API-------------------
+
+module.exports = rutas; //para exportar y usar en otro lado
---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express') //llamamos a Express
+const bodyParser = require('body-parser');//------------------
+
+
+var app = express()   
+app.use(bodyParser.json())
+//-------------           
+var port = process.env.PORT || 8000  // establecemos nuestro puerto
+//-- para dar accesos desde cualquier servidor
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
+//Routes llama a los controladores
+app.use('/', require('./controller/novedadController'));
+
+  // iniciamos nuestro servidor
+app.listen(port)
+console.log('Servidor NodeJs Runing en http://localhost:' + port);
+console.log('Para terminar presione las teclas Ctrl+C');
